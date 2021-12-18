8e8450f681db607fba9f750388a458a8e773aed4
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-12-19 00:44:21 +0100

    day 04

--- AdventOfTest/TestDays.cs
+++ AdventOfTest/TestDays.cs
@@ -14,6 +14,7 @@ public class TestDays
             yield return new TestCaseData(new Day_01(), "1557", "1608");
             yield return new TestCaseData(new Day_02(), "1882980", "1971232560");
             yield return new TestCaseData(new Day_03(), "3277364", "5736383");
+            yield return new TestCaseData(new Day_04(), "63424", "23541");
         }
     }
     [TestCaseSource(nameof(AdventOfCodeDays))]
