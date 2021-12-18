8e05a05c296086a722f4ee7a549bff91552028fc
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-12-18 22:10:19 +0100

    Add unit tests for days

---
+++ AdventOfTest/TestDays.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using AdventOfCode;
+using AoCHelper;
+using NUnit.Framework;
+
+namespace AdventOfTest;
+
+public class TestDays
+{
+    public static IEnumerable<TestCaseData> AdventOfCodeDays
+    {
+        get
+        {
+            yield return new TestCaseData(new Day_01(), "1557", "1608");
+            yield return new TestCaseData(new Day_02(), "1882980", "1971232560");
+            yield return new TestCaseData(new Day_03(), "3277364", "5736383");
+        }
+    }
+    [TestCaseSource(nameof(AdventOfCodeDays))]
+    public void TestDay(BaseDay day, string solution1, string solution2)
+    {
+        Assert.AreEqual(day.Solve_1().Result, solution1);
+        Assert.AreEqual(day.Solve_2().Result, solution2);
+    }
+}
