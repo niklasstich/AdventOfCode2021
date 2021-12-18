beb2b7d910c3f8367cb25e77d4f7ef47d555bd64
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-12-18 16:19:56 +0100

    introduce proper program class with main method

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,14 +1,33 @@
-﻿if (args.Length == 0)
-{
-    Solver.SolveLast(new SolverConfiguration { ClearConsole = false });
-}
-else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
-{
-    Solver.SolveAll(new SolverConfiguration { ShowConstructorElapsedTime = true, ShowTotalElapsedTimePerDay = true });
-}
-else
-{
-    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+﻿namespace AdventOfCode;
 
-    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    Solver.SolveLast(new SolverConfiguration { ClearConsole = false });
+                    break;
+                case 1 when args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase):
+                    Solver.SolveAll(new SolverConfiguration
+                        { ShowConstructorElapsedTime = true, ShowTotalElapsedTimePerDay = true });
+                    break;
+                default:
+                {
+                    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+
+                    Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+                    break;
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Environment.FailFast("Solver execution failed", ex);
+        }
+    }
 }
