6ecd20602e9f9c8ca443803347e4a0ffa5341022
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2021-12-19 13:43:08 +0100

    day 05

---
+++ AdventOfCode/Day_05.cs
@@ -0,0 +1,76 @@
+namespace AdventOfCode;
+
+public class Day_05 : BaseDay
+{
+    private readonly string _input;
+    private readonly string[] _lines;
+
+    public Day_05()
+    {
+        _input = File.ReadAllText(InputFilePath);
+        _lines = _input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public override ValueTask<string> Solve_1() => new(PuzzleOne());
+
+    public override ValueTask<string> Solve_2() => new(PuzzleTwo());
+
+    private string PuzzleOne()
+    {
+        return GetHydrothermalMap(false).Count(num => num > 1).ToString();
+    }
+
+    private string PuzzleTwo()
+    {
+        return GetHydrothermalMap(true).Count(num => num > 1).ToString();
+    }
+
+    private IEnumerable<int> GetHydrothermalMap(bool diagonals)
+    {
+        var field = new int[1000*1000];
+        foreach (var line in _lines)
+        {
+            var numbers = line.Split(new []{',', ' ', '-', '>'}, StringSplitOptions.RemoveEmptyEntries);
+            int x1, y1, x2, y2;
+            (x1, y1, x2, y2) = (Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1]), Convert.ToInt32(numbers[2]),
+                Convert.ToInt32(numbers[3]));
+            if (!(x1 == x2 || y1 == y2))
+            {
+                if (!diagonals) continue;
+                var len = Math.Abs(x1 - x2) + 1;
+                var xseq = Enumerable.Range(Math.Min(x1, x2), len).ToArray();
+                var yseq = Enumerable.Range(Math.Min(y1, y2), len).ToArray();
+                if (x1 < x2 && y1 > y2 || x1 > x2 && y1 < y2)
+                {
+                    yseq = yseq.Reverse().ToArray();
+                }
+
+                for (var i = 0; i < xseq.Length; i++)
+                {
+                    var x = xseq[i];
+                    var y = yseq[i];
+                    field[y * 1000 + x]++;
+                }
+                continue;
+            }
+            
+            //swap numbers
+            if (x1 > x2 || y1 > y2)
+            {
+                (x1, x2) = (x2, x1);
+                (y1, y2) = (y2, y1);
+            }
+
+            for (var column = x1; column <= x2; column++)
+            {
+                for (var row = y1; row <= y2; row++)
+                {
+                    field[row * 1000 + column]++;
+                }
+            }
+
+        }
+
+        return field;
+    }
+}
--- AdventOfTest/TestDays.cs
+++ AdventOfTest/TestDays.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using AdventOfCode;
 using AoCHelper;
 using NUnit.Framework;
@@ -15,12 +16,13 @@ public class TestDays
             yield return new TestCaseData(new Day_02(), "1882980", "1971232560");
             yield return new TestCaseData(new Day_03(), "3277364", "5736383");
             yield return new TestCaseData(new Day_04(), "63424", "23541");
+            yield return new TestCaseData(new Day_05(), "6564", "19172");
         }
     }
     [TestCaseSource(nameof(AdventOfCodeDays))]
-    public void TestDay(BaseDay day, string solution1, string solution2)
+    public async Task TestDay(BaseDay day, string solution1, string solution2)
     {
-        Assert.AreEqual(day.Solve_1().Result, solution1);
-        Assert.AreEqual(day.Solve_2().Result, solution2);
+        Assert.AreEqual(solution1, await day.Solve_1());
+        Assert.AreEqual(solution2, await day.Solve_2());
     }
 }
